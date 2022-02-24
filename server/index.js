fb53e338bceefc451000c0b59e5f5f77462402bc
Lagrato <thiagobacaxi8@gmail.com>
2022-02-24 15:34:23 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const socket = require('socket.io');
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const io = new socket.Server(server);
+const VIBRATION_CHECK_TIME = 10000;
+
+let userCount = 0;
+let vibrating = 0;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html');
+})
+
+io.on('connection', socket => {
+    userCount++;
+    io.emit('userCount', {userCount})
+    io.emit('vibrationUpdate',{vibrating})
+    socket.on('vibration', msg => {
+        io.emit('vibration', msg)
+    });
+    socket.on('disconnect', () => {
+        userCount--;
+        vibrating = 0;
+        io.emit('vibrationCheck', {});
+        io.emit('userCount', {userCount})
+    })
+    socket.on('vibrationIncrease', () => {
+        vibrating++;
+        io.emit('vibrationUpdate',{vibrating})
+    })
+    socket.on('vibrationDecrease', () => {
+        vibrating--;
+        io.emit('vibrationUpdate',{vibrating})
+    })
+});
+
+server.listen(3000, () => {
+    console.log('listening');
+});
